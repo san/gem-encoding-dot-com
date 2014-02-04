552524013c1cbff3686dddc5bca1d0c15e3c6237
Syed Aamir Naqib <san@ximda.com>
2014-02-04 17:56:26 -0600

    Added wowza_multibitrate as valid output format

--- lib/encoding_dot_com/video_format.rb
+++ lib/encoding_dot_com/video_format.rb
@@ -1,6 +1,6 @@
 module EncodingDotCom
   class VideoFormat < Format #:nodoc:
-    ALLOWED_OUTPUT_FORMATS = %w{flv fl9 wmv 3gp mp4 m4v ipod iphone iphone_stream ipad ipad_stream appletv psp zune mp3 wma}.freeze
+    ALLOWED_OUTPUT_FORMATS = %w{flv fl9 wmv 3gp mp4 m4v ipod iphone iphone_stream ipad ipad_stream appletv psp zune mp3 wma wowza_multibitrate}.freeze
     
     allowed_attributes :output, :size, :bitrate, :framerate, :framerates, :video_codec, :audio_bitrate, :audio_sample_rate, :audio_codec, :audio_channels_number, :profile,
       :audio_volume, :maxrate, :minrate, :bufsize, :keyframe, :keyframes, :start, :duration, :rc_init_occupancy, :crop_top, :crop_left, :crop_right, :crop_bottom,
